74d23df9baec139ea9b86f04bccc852e8bfc9d66
aalbertohh <99363418+aalbertohh@users.noreply.github.com>
2022-11-02 13:08:08 +0200

    Update home.java

--- Proiect  final/src/home.java
+++ Proiect  final/src/home.java
@@ -17,6 +17,100 @@ public  class home {
     private JButton btnExit;
     private JButton btnClear;
     private JButton btnEnter;
+    
+     public home() {
+        radSpania.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                if(radSpania.isSelected())
+                    radBulgaria.setSelected(false);
+                radGermania.setSelected(false);
+
+
+            }
+        });
+        radBulgaria.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                if(radBulgaria.isSelected())
+                    radSpania.setSelected(false);
+                radGermania.setSelected(false);
+            }
+        });
+        radGermania.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(radGermania.isSelected())
+                    radBulgaria.setSelected(false);
+                radSpania.setSelected(false);
+            }
+        });
+        btnEnter.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                DecimalFormat df = new DecimalFormat("0.00");
+
+                double total;
+                double subtotal= 0;
+                final double TvaRate=0.02;
+                double tva;
+
+                if(radSpania.isSelected()){
+                    subtotal = subtotal + 150.0;
+                }else if(radBulgaria.isSelected()){
+                    subtotal = subtotal + 50.0;
+                }else if(radGermania.isSelected()){
+                    subtotal = subtotal + 100.0;
+                }
+                if(chkBussinesClass.isSelected()){
+                    subtotal = subtotal + 200.0;
+                }if(chkBagajDeCala.isSelected()){
+                    subtotal = subtotal + 50.0;
+                }if(chkMancareLaBord.isSelected()){
+                    subtotal = subtotal + 25.0;
+                }
+
+                txtSub.setText(Double.toHexString(subtotal));
+
+                subtotal = Double.parseDouble(txtSub.getText());
+                tva = subtotal * TvaRate;
+                total = tva + subtotal;
+
+                txtTax.setText(Double.toString(tva));
+                txtTotal.setText(Double.toString(total));
+
+                txtTax.setText(df.format(tva));
+                txtSub.setText(df.format(subtotal));
+                txtTotal.setText(df.format(total));
+            }
+        });
+        btnExit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        btnClear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                radSpania.setSelected(false);
+                radGermania.setSelected(false);
+                radBulgaria.setSelected(false);
+                chkBussinesClass.setSelected(false);
+                chkMancareLaBord.setSelected(false);
+                chkBagajDeCala.setSelected(false);
+                txtTotal.setText("");
+                txtSub.setText("");
+                txtTax.setText("");
+
+            }
+        });
+    }
+
+    public JPanel getPanel1(){
+        return panel1;
 
 }
 
