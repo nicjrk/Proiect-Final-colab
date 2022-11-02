13de04aaba25ba81e77931e5f40c65c43b011235
nicjrk <116735056+nicjrk@users.noreply.github.com>
2022-11-02 12:53:27 +0200

    Add files via upload

---
+++ Proiect  final/src/home.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+
+public  class home {
+    private JPanel panel1;
+    private JRadioButton radGermania;
+    private JRadioButton radBulgaria;
+    private JRadioButton radSpania;
+    private JCheckBox chkBussinesClass;
+    private JCheckBox chkBagajDeCala;
+    private JCheckBox chkMancareLaBord;
+    private JTextPane txtSub;
+    private JTextPane txtTax;
+    private JTextPane txtTotal;
+    private JButton btnExit;
+    private JButton btnClear;
+    private JButton btnEnter;
+
+}
+
+
---
+++ Proiect  final/src/login.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class login {
+    private JLabel Username;
+    private JPanel panel;
+    private JPasswordField password;
+    private JTextField textField1;
+    private JButton Login;
+
+    public JPasswordField getPassword(){
+        return password;
+    }
+
+
+
+    public login() {
+
+        Login.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               String pwd = new String(getPassword().getPassword());
+
+               if(pwd.equals(ExSingleton.getInstance().modificaCamp1("parola")))
+                {
+
+                    JOptionPane.showMessageDialog(null, "Te ai logat cu succes");
+                    panel.setVisible(false);
+                    JFrame avion = new JFrame();
+                    avion.setContentPane(new home().getPanel1());
+                    avion.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    avion.pack();
+                    avion.setVisible(true);
+
+                }
+                else {
+                    JOptionPane.showMessageDialog(null,"Parola o fost introdusa gresit or username-ul este incorect");
+                }
+            }
+        });
+    }
+
+
+    public static void main(String[] args){
+            JFrame frame = new JFrame("App");
+            frame.setContentPane(new login().panel);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setVisible(true);
+
+
+    }
+}
